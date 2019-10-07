1f3511b4723137fc535622ce64ca7b35e8278e7b
がくかんせつしょう <Heislandmine@protonmail.com>
2019-10-07 18:32:31 +0900

    リファクタリング

--- HighAndLow/HighAndLow.py
+++ HighAndLow/HighAndLow.py
@@ -14,6 +14,7 @@ class Dealer:
 
 class Agent:
     def __init__(self, p_type=0):
+        self.predict_num = 0
         # 行動計画を生成　0 = 低い, 1 = 高い, 2 = ランダム
         if p_type == 0:
             self.act_plan = [1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0]
@@ -27,15 +28,19 @@ class Agent:
     def predict(self, open_card):
         plan = self.act_plan[open_card - 1]
         if plan == 2:
-            return random.choice([0, 1])
+            self.predict_num = random.choice([0, 1])
 
         else:
-            return plan
+            self.predict_num = plan
 
 
 class HighAndLow:
     def __init__(self):
         self.count = 0
+        self.open_card = 0
+        self.next_card = 0
+        self.result = 0
+        self.eval_result = 0
         self.dl = None
         self.ag = None
 
@@ -44,34 +49,34 @@ class HighAndLow:
 
     def card_comp(self, c_card, n_card):
         if c_card < n_card:
-            return 1
+            self.result = 1
         elif c_card > n_card:
-            return 0
+            self.result = 0
         else:
-            return 2  # 2枚のカードが同じ場合
+            self.result = 2  # 2枚のカードが同じ場合
 
     def eval(self, p_result, result):
         if p_result == result:
             self.count += 1
-            return 0
+            self.eval_result = 0
         else:
-            return 1
+            self.eval_result = 1
 
     def play_game(self, dl, ag, first_card):
-        open_card = first_card
+        self.open_card = first_card
         while True:
-            predict_result = ag.predict(open_card)
-            next_card = dl.draw_card()
-            result = self.card_comp(open_card, next_card)
+            ag.predict(self.open_card)
+            self.next_card = dl.draw_card()
+            self.card_comp(self.open_card, self.next_card)
 
-            if result == 2:
-                open_card = next_card
+            if self.result == 2:
+                self.open_card = self.next_card
             else:
-                eval_result = self.eval(predict_result, result)
-                if eval_result == 1:
+                self.eval(ag.predict_num, self.result)
+                if self.eval_result == 1:
                     break
                 else:
-                    open_card = next_card
+                    self.open_card = self.next_card
 
             if len(dl.deck) == 0:
                 break
@@ -79,7 +84,7 @@ class HighAndLow:
     def high_and_low(self, dl, ag):
         self.dl = dl
         self.ag = ag
-        first_card = self.open_first_card(self.dl)
+        first_card = dl.draw_card()
         self.play_game(self.dl, self.ag, first_card)
 
         return self.count
